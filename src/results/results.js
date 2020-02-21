6a25df08a4b1ec7ad20ae32af305239df772bc3b
Bryce Eklund <bryce.eklund@gmail.com>
2020-02-20 19:11:57 -0800

    twitter share link

--- src/results/results.js
+++ src/results/results.js
@@ -184,7 +184,7 @@ const Results = (props) => {
               data-href={`https://ytpoet.now.sh/poem/${props.match.url.split('/')[2]}`}
               data-layout="button">
             </div>
-            <a href={`https://twitter.com/intent/tweet?text=${props.match.url}`} class={`twitter-share-button ${options ? 'hidden' : ''}`} data-show-count="false">
+            <a href={`https://twitter.com/intent/tweet?text=https://ytpoet.now.sh${props.match.url}`} class={`twitter-share-button ${options ? 'hidden' : ''}`} data-show-count="false">
               twitter</a>
             
             <button onClick={() => downloadPoem()}>download</button>
