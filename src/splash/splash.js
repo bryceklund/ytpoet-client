42510db62ce13709e9f26fe029b4f9053af57cbb
Bryce Eklund <bryce.eklund@gmail.com>
2020-02-23 14:40:50 -0800

    write up some basic instructions

--- src/splash/splash.js
+++ src/splash/splash.js
@@ -6,7 +6,13 @@ const Splash = () => {
         <div>
             <p className='instructions'>
                 [welcome instructions]<br />
-                Hello and welcome to ytPoet! To get started, paste a link in the box and hit "GO". Don't actually paste a link in there though, this is just a beta. You can hit "GO" though.
+                Hello and welcome to ytPoet!<br />
+                To get started, paste a YouTube video in the text box<br />
+                Next, choose your poem type from the drop-down menu<br />
+                Finally, hit "GO" to generate a poem<br />
+                Click "generate link" to save your poem and make it sharable<br />
+                Once generated, use a social share link to post it to Facebook or Twitter<br />
+                At any given moment, click the "random poem" button to be taken to a random saved poem<br />
             <br />
             </p>
             <div className='continue-container'>
