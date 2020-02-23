cec2798b8bb1c9e945b3044a64241ce087ab592c
Bryce Eklund <bryce.eklund@gmail.com>
2020-02-23 13:18:05 -0800

    attempt fb share button

--- src/results/results.js
+++ src/results/results.js
@@ -179,7 +179,7 @@ const Results = (props) => {
             </div>
           </div>
           <div className='share-buttons'>
-            <FacebookShareButton><FacebookIcon size={32} round={false} /></FacebookShareButton>
+            <FacebookShareButton url={`https://ytpoet.now.sh${props.match.url}`} quote='check out my poem on #ytpoet: '><FacebookIcon size={32} round={false} /></FacebookShareButton>
             <TwitterShareButton url={`https://ytpoet.now.sh${props.match.url}`} options={{ text: 'check out my poem on #ytpoet: ' }} />
             <button onClick={() => downloadPoem()}>download</button>
             <button onClick={() => copyLink === 'new' ? savePoem() : toClipBoard(props.match.url.split('/')[2])}>generate link</button>
